06aa5a2389815e50a92eab819fe2dadf78e9dc4b
Sosme400 <149852662+Sosme400@users.noreply.github.com>
2024-09-22 22:39:42 -0600

    Se añadio el archivo  vercel.json en el proyecto de la tienda

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,9 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist',   // Directorio donde se generará el build
+    outDir: 'dist',   // Donde se generará el build
     rollupOptions: {
-      input: './index.html',  // Asegúrate de que la ruta esté correcta
+      input: './index.html',  // Asegúrate de que apunte al archivo index.html correcto
     },
   },
 });
